ce9c6da4ce9bd5caae6f3832901b6f971c31e090
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-12 00:13:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,31 @@ number += 3 //8
 number -= 2 // 3
 number *= 10 // 50
 number /= 5 // 1
+var number = 10
+ 
+function add5() {
+  number += 5
+}
+ 
+function divideBy3() {
+  number /= 3
+}
+ 
+divideBy3()
+ 
+console.log(number) // 3.333333333335
+ 
+add5()
+ 
+console.log(number) // 8.333333333335
+ 
+// reset number
+number = 10
+ 
+add5()
+ 
+console.log(number) // 15
+ 
+divideBy3()
+ 
+console.log(number) // 5

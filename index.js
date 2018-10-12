113b332b3594f59524e705632dec716e3298513a
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-12 00:20:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,46 +1,13 @@
-1 + 80 // 81
-60 - 40 // 20
-2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
-5.0 / 2.5 // 2
 
-var number = 605
- 
-number++ // 5... hmmmm
- 
-number // 6 -- the number was incremented after it was evaluated
- 
-number-- // 6
- 
-number // 5
-number += 3 //8
-number -= 2 // 3
-number *= 10 // 50
-number /= 5 // 1
-var number = 10
- 
-function add() {
-  number += 5
+function add(){
+  
 }
- 
-function divideBy3() {
-  number /= 3
+function subtract(){
+  
 }
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
+function mutiply(){
+  
+}
+function divide(){
+  
+}
